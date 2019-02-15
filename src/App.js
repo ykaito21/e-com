925cc110bd3ee630a06b49a0b2344d016e636ae6
Yuki Kaito <ykaito21@gmail.com>
2019-02-15 21:10:22 +0900

    add route and structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+
+// import logo from "./logo.svg";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +15,13 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>good morning</h1>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Product 1</h3>
+      </div>
+    );
+  }
+}
+
+export default Product;
